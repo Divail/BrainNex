1961709772067b7f237b868f0ab1f1279cd60313
thelegendmaster64 <slavko.stevi@selu.edu>
2023-09-22 19:51:48 -0500

    Updated back.py
    Updated backend with all working functions. Needs to be tested on the frontend

--- back.py
+++ back.py
@@ -1,131 +1,119 @@
 import mne
-import numpy as np
 import matplotlib.pyplot as plt
-from mne import Epochs, pick_types, events_from_annotations
 from mne.channels import make_standard_montage
-from mne.io import concatenate_raws, read_raw_edf
 from mne.datasets import eegbci
-from mne.preprocessing import ICA
-from mne import io
-from mne.datasets import sample
-from mne.minimum_norm import read_inverse_operator, compute_source_psd
 
-# print(__doc__)
-
-# #############################################################################
-# # Set parameters and read data
-
-# avoid classification of evoked responses by using epochs that start 1s after
-# cue onset.
-if __name__ == "__main__":
-    tmin, tmax = -1.0, 4.0
-    event_id = dict(hands=2, feet=3)
-    subject = 1
-    runs = [6, 10, 14]  # motor imagery: hands vs feet
-
-    raw_fnames = eegbci.load_data(subject, runs)
-    raw = concatenate_raws([read_raw_edf(f, preload=True) for f in raw_fnames])
-    eegbci.standardize(raw)  # set channel names
-    montage = make_standard_montage("standard_1005")
-    raw.set_montage(montage)
-
-    # Apply band-pass filter
-    raw.filter(7.0, 30.0, fir_design="firwin", skip_by_annotation="edge")
-
-    events, _ = events_from_annotations(raw, event_id=dict(T1=2, T2=3))
-
-    picks = pick_types(
-        raw.info, meg=False, eeg=True, stim=False, eog=False, exclude="bads"
-    )
-    spectrum = raw.compute_psd()
-    spectrum.plot(average=True, picks="data", exclude="bads")
-
-    # Plot a power spectral density of raw data for a specific channel(s)
-    midline = ["Fp1"]
-    spectrum.plot(picks=midline, exclude="bads")
-
-    # Plot sensor (electrode) locations on a head
-    raw.plot_sensors(ch_type="eeg")
-
-    montage.plot()
-
-    # Plot topomap (heatmap) of power spectral density of raw data
-    raw.compute_psd().plot_topomap()
-
-    # Apply ICA to Raw data
-    ica = mne.preprocessing.ICA(n_components=20, random_state=97, max_iter=800)
-    ica.fit(raw)
-    ica.exclude = [15]  # ICA components
-    ica.plot_properties(raw, picks=ica.exclude)
-
-    #    Find the covariance of channels of raw data and plot
-    noise_cov = mne.compute_raw_covariance(raw, method="shrunk")
-    fig_noise_cov = mne.viz.plot_cov(noise_cov, raw.info, show_svd=False)
-
-    # Plot ICA components
-    mne.viz.plot_ica_sources(ica, raw)
-    ica.plot_components()
-    ica.plot_overlay(raw)
-
-
-
-def preprocessing_ICA(raw):
-    # Apply ICA to Raw data
-    ica = mne.preprocessing.ICA(n_components=20, random_state=97, max_iter=800)
-    ica.fit(raw)
-    ica.exclude = [15]  # ICA components
-    ica.plot_properties(raw, picks=ica.exclude)                                                          
-
-    # Find the covariance of channels of raw data and plot
-    #noise_cov = mne.compute_raw_covariance(raw, method="shrunk")
-    #fig_noise_cov = mne.viz.plot_cov(noise_cov, raw.info, show_svd=False)
-
-    # Plot ICA components
-    mne.viz.plot_ica_sources(ica, raw)
-    ica.plot_components()
-    ica.plot_overlay(raw)
+class EEG():
+    ica_bool = False # True if ICA preprocessing has been applied
     
-    return ica
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.raw = self.load_edf_data(file_path)
+        self.montage() # Instantiate montage
+        
+    # Loading the EDF raw data
+    def load_edf_data(self, file_path):
+        # Load the .edf data from the file and return it as raw data
+        raw = mne.io.read_raw_edf(file_path, preload=True)
+        return raw
     
-def power_spectral_density_PSD(raw):
-    # Compute the power spectral density of raw data and plot
-    spectrum = raw.compute_psd()
-    spectrum.plot(average=True, picks="data", exclude="bads")
+    # Reseting the raw file 
+    def reset_raw(self):
+        # Reloading the raw file in order to reset it
+        self.raw = self.load_edf_data(self.file_path)
+        self.montage()
+    
+    # Set Montage
+    def montage(self):
+        # Needs work
+        eegbci.standardize(self.raw)  # Set channel names
+        montage = make_standard_montage("standard_1005")
+        self.raw.set_montage(montage)
+        
+    # PSD
+    def power_spectral_density(self):
+        # Compute the power spectral density of raw data and plot
+        self.raw.compute_psd().plot(average = True, picks = 'data', exclude = 'bads')
+        
+        plt.show()
 
-    # Plot a power spectral density of raw data for a specific channel(s)
-    midline = ["Fp1"]
-    spectrum.plot(picks=midline, exclude="bads")
-    
-def topomap_PSD(raw):
-    # Plot topomap (heatmap) of power spectral density of raw data
-    raw.compute_psd().plot_topomap()
+    # PSD - Channel(s)
+    def power_spectral_density_channels(self, picks = None):
+        # Plot a power spectral density of raw data for a specific channel(s)
+        self.raw.compute_psd().plot(picks = picks, exclude = 'bads')
+        
+        plt.show()
 
-def bandpass_filtering(raw):
-    # Apply band-pass filter
-    low = 7.0 # Low frq limit
-    high = 30.0 # High frq limit
-    firDesign = 'firwin'
-    skip_byan = "edge"
-    raw.filter(low, high, fir_design=firDesign, skip_by_annotation=skip_byan)
-    
+    # PSD - Topomap
+    def topomap_PSD(self):
+        # Plot topomap (heatmap) of power spectral density of raw data
+        self.raw.compute_psd().plot_topomap()
+        
+        plt.show()
 
-def low_pass_filtering(raw):
-    low = None # Low frq limit
-    high = 50. # High frq limit
-    firDesign = 'firwin' # firwin - default, additional ones are: firwin2, firwin3
-    raw.filter(low, high, fir_design=firDesign)
+    # Filtering - Band-pass
+    def bandpass_filtering(self, low_frq = 7.0, high_frq = 13.0, fir_design = 'firwin', skip_by_annonation = 'edge'):
+        # Apply band-pass filter
+        self.raw.filter(low_frq, high_frq, fir_design = fir_design, skip_by_annotation = skip_by_annonation)
+        
+        self.raw.plot()
+        plt.show()
 
-def high_pass_filtering(raw):
-    low = 1. # Low frq limit
-    high = None # High frq limit
-    firDesign = 'firwin' # firwin - default, additional ones are: firwin2, firwin3
-    raw.filter(low, high, fir_design=firDesign)
+    # Filtering - Low pass    
+    def lowpass_filtering(self, high_frq = 30.0, fir_design = 'firwin'):
+        low_frq = None
+        self.raw.filter(low_frq, high_frq, fir_design = fir_design)
+        
+        self.raw.plot()
+        plt.show()
+        
+    # Filtering - High pass
+    def highpass_filtering(self, low_frq = 20.0, fir_design = 'firwin'):
+        high_frq = None # High frq limit
+        self.raw.filter(low_frq, high_frq, fir_design = fir_design)  
+        
+        self.raw.plot()
+        plt.show()
     
-def plot_each_ICA_component_as_1D_time_series(raw, picks): # picks = [], picks will be a list 
-    ica = preprocessing_ICA(raw)
-    mne.viz.plot_ica_components(ica, picks)
-    #ica.plot_components(picks)
+    # Electrodes   
+    def plot_electrode(self):
+        # Plot sensor (electrode) locations on a head
+        self.raw.plot_sensors(ch_type="eeg")
+        
+        plt.show()
     
-def plot_electrode(raw):
-    # Plot sensor (electrode) locations on a head
-    raw.plot_sensors(ch_type="eeg")
+    # ICA - Preprocessing
+    def preprocessing_ICA(self, n_components = 20, random_state = 97, max_iter = 800):
+        # Apply ICA to Raw data
+        self.ica = mne.preprocessing.ICA(n_components = n_components, random_state = random_state, max_iter = max_iter)
+        self.ica.fit(self.raw)
+        self.ica_bool = True
+        #self.ica.exclude = [15]  # ICA components
+    
+    # ICA - Components 1D Series
+    def plot_ica_components_1D(self, picks = None):
+        # Plot the 1D time series of each ICA component
+        if self.ica_bool == True:
+            self.ica.plot_sources(self.raw, picks = picks, title = 'ICA - Components 1D Series')
+        
+            plt.show()
+        else:
+            print("Plase apply ICA Preprocessing")
+        
+    # ICA - Components Topomap
+    def plot_ica_components_topomap(self, picks = None):
+        if self.ica_bool == True:
+            # Plot Ica Components Topomap
+            self.ica.plot_components(picks = picks, title = 'ICA Components Topomap')
+            
+            plt.show()
+        else:
+            print("Plase apply ICA Preprocessing")
+            
+    # ICA - Components' Properties
+    def plot_ica_components_properties(self, picks = None):
+        if self.ica_bool == True:
+            # Plot ICA components' Properties
+            self.ica.plot_properties(self.raw, picks = picks) #picks = self.ica.exclude
+        else:
+            print("Plase apply ICA Preprocessing")    
--- backTESTING.py
+++ backTESTING.py
@@ -1,23 +1,38 @@
 import mne
-import numpy as np
 import matplotlib.pyplot as plt
-from mne import Epochs, pick_types, events_from_annotations
+from mne import pick_types
 from mne.channels import make_standard_montage
 from mne.io import concatenate_raws, read_raw_edf
 from mne.datasets import eegbci
-from mne.preprocessing import ICA
-from mne import io
-from mne.datasets import sample
-from mne.minimum_norm import read_inverse_operator, compute_source_psd
 
 class EEG():
-    def __init__(self, raw):
+    ica_bool = False # True if ICA preprocessing has been applied
+    
+    def __init__(self, raw): #file_path):
         self.raw = raw
-        self.rawLow = None
-        self.rawHigh = None
-        self.rawBand = None
-        self.ica = None
-
+        #self.file_path = file_path
+        #self.raw = self.load_edf_data(file_path)
+        #self.montage() # Instantiate montage
+        
+    # Loading the EDF raw data
+    def load_edf_data(self, file_path):
+        # Load the .edf data from the file and return it as raw data
+        raw = mne.io.read_raw_edf(file_path, preload=True)
+        return raw
+    
+    # Reseting the raw file 
+    def reset_raw(self):
+        # Reloading the raw file in order to reset it
+        self.raw = self.load_edf_data(self.file_path)
+        self.montage()
+    
+    # Set Montage
+    def montage(self):
+        # Needs work
+        eegbci.standardize(self.raw)  # Set channel names
+        montage = make_standard_montage("standard_1005")
+        self.raw.set_montage(montage)
+        
     # PSD
     def power_spectral_density(self):
         # Compute the power spectral density of raw data and plot
@@ -26,7 +41,7 @@ class EEG():
         plt.show()
 
     # PSD - Channel(s)
-    def power_spectral_density_channels(self, picks):
+    def power_spectral_density_channels(self, picks = None):
         # Plot a power spectral density of raw data for a specific channel(s)
         self.raw.compute_psd().plot(picks = picks, exclude = 'bads')
         
@@ -75,94 +90,40 @@ class EEG():
         # Apply ICA to Raw data
         self.ica = mne.preprocessing.ICA(n_components = n_components, random_state = random_state, max_iter = max_iter)
         self.ica.fit(self.raw)
+        self.ica_bool = True
         #self.ica.exclude = [15]  # ICA components
     
     # ICA - Components 1D Series
     def plot_ica_components_1D(self, picks = None):
         # Plot the 1D time series of each ICA component
-        self.ica.plot_sources(self.raw, picks = picks, title = 'ICA - Components 1D Series')
+        if self.ica_bool == True:
+            self.ica.plot_sources(self.raw, picks = picks, title = 'ICA - Components 1D Series')
         
-        plt.show()
+            plt.show()
+        else:
+            print("Plase apply ICA Preprocessing")
         
     # ICA - Components Topomap
     def plot_ica_components_topomap(self, picks = None):
-        # Plot Ica Components Topomap
-        self.ica.plot_components(picks = picks, title = 'ICA Components Topomap')
-        
-        plt.show()
-        
-        
-    #self.ica.plot_properties(self.raw, picks = self.ica.exclude)                                                                                                               
+        if self.ica_bool == True:
+            # Plot Ica Components Topomap
+            self.ica.plot_components(picks = picks, title = 'ICA Components Topomap')
+            
+            plt.show()
+        else:
+            print("Plase apply ICA Preprocessing")
+            
+    # ICA - Components' Properties
+    def plot_ica_components_properties(self, picks = None):
+        if self.ica_bool == True:
+            # Plot ICA components' Properties
+            self.ica.plot_properties(self.raw, picks = picks) #picks = self.ica.exclude
+        else:
+            print("Plase apply ICA Preprocessing")                                                                                                           
 
     # Find the covariance of channels of raw data and plot
     #noise_cov = mne.compute_raw_covariance(raw, method="shrunk")
     #fig_noise_cov = mne.viz.plot_cov(noise_cov, raw.info, show_svd=False)
-
-    # Plot ICA components
-    #mne.viz.plot_ica_sources(self.ica, self.raw)
-    #self.ica.plot_components()
-    #self.ica.plot_overlay(self.raw)
-    
-'''
-# PSD
-def power_spectral_density(raw):
-    # Compute the power spectral density of raw data and plot
-    raw.compute_psd().plot(average=True, picks="data", exclude="bads")
-    
-    plt.show()
-
-# PSD - Channel(s)
-def power_spectral_density_channels(raw, picks):
-    # Plot a power spectral density of raw data for a specific channel(s)
-    raw.compute_psd().plot(picks=picks, exclude="bads")
-    
-    plt.show()
-
-# PSD - Topomap
-def topomap_PSD(raw):
-    # Plot topomap (heatmap) of power spectral density of raw data
-    raw.compute_psd().plot_topomap()
-
-# Filtering - Band-pass
-def bandpass_filtering(raw):
-    # Apply band-pass filter
-    low = 7.0 # Low frq limit
-    high = 30.0 # High frq limit
-    firDesign = 'firwin'
-    skip_byan = "edge"
-    raw.filter(low, high, fir_design=firDesign, skip_by_annotation=skip_byan)
-    
-    raw.plot()
-    plt.show()
-
-# Filtering - Low pass    
-def low_pass_filtering(raw):
-    low = None # Low frq limit
-    high = 50. # High frq limit
-    firDesign = 'firwin' # firwin - default, additional ones are: firwin2, firwin3
-    raw.filter(low, high, fir_design=firDesign)
-    
-    raw.plot()
-    plt.show()
-    
-# Filtering - High pass
-def high_pass_filtering(raw):
-    low = 1. # Low frq limit
-    high = None # High frq limit
-    firDesign = 'firwin' # firwin - default, additional ones are: firwin2, firwin3
-    raw.filter(low, high, fir_design=firDesign)  
-    
-    
-    raw.plot()
-    plt.show()
- 
-# Electrodes   
-def plot_electrode(raw):
-    # Plot sensor (electrode) locations on a head
-    raw.plot_sensors(ch_type="eeg")
-    
-    plt.show()
-'''
 
 if __name__ == "__main__":
     tmin, tmax = -1.0, 4.0
@@ -172,9 +133,9 @@ if __name__ == "__main__":
 
     raw_fnames = eegbci.load_data(subject, runs)
     raw = concatenate_raws([read_raw_edf(f, preload=True) for f in raw_fnames])
-    
+
     eeg = EEG(raw)
-    
+
     #################################################
     eegbci.standardize(raw)  # set channel names
     montage = make_standard_montage("standard_1005")
@@ -192,12 +153,15 @@ if __name__ == "__main__":
     eeg.topomap_PSD()
     
     eeg.plot_electrode()
-    
+
     picks2 = ["AFz", "CPz"]
     
     eeg.preprocessing_ICA()
+    eeg.plot_ica_components_properties([18,11,17])
+    
     eeg.plot_ica_components_1D()
-    eeg.plot_ica_components_topomap()
+    eeg.plot_ica_components_topomap([0,6,7])
+    eeg.plot_ica_components_properties()
     
     eeg.power_spectral_density()
     eeg.power_spectral_density_channels(picks2)
@@ -205,7 +169,12 @@ if __name__ == "__main__":
     eeg.lowpass_filtering()
     
     eeg.power_spectral_density()
+    eeg.reset_raw()
+    eeg.power_spectral_density()
+    eeg.preprocessing_ICA()
     eeg.plot_ica_components_1D()
+    eeg.power_spectral_density()
+    eeg.plot_electrode()
     eeg.power_spectral_density_channels(picks2)
     
     eeg.highpass_filtering()
